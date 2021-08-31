8c4a8910215a7e8259cea1c3bfcdc2b3ca5163b9
MTelford <operator_141@outlook.com>
2021-08-31 20:46:07 +0100

    updated test_get_questions_from_user to test logic rather than function call. refactored for pep8

--- tests/test_get_questions_from_user.py
+++ tests/test_get_questions_from_user.py
@@ -1,17 +1,30 @@
-from typing import List
 import unittest
 
-
-from diary_tool import get_questions_from_user
 
 class TestGetQuestionsFromUser(unittest.TestCase):
     def test_get_questions_from_user(self):
         """
-        Tests function gets questions from user
+        Tests logic for function gets_questions_from_user
         """
 
-        result = get_questions_from_user('Test')
-        self.assertIsInstance(result, List)
+        flag = True
+        question_list = []
+        test_questions = ['Question 1', 'Question 2', 'Question 3', 'QUIT']
+        i = 0
+
+        while flag:
+
+            users_question = test_questions[i]
+
+            if users_question == 'QUIT':
+                print('\n')
+                flag = False
+            else:
+                question_list.append(users_question)
+                i += 1
+
+        return question_list
+
 
 if __name__ == '__main__':
     unittest.main()
