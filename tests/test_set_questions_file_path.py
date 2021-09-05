042ac495d359bf7e34d372088013b5f2ad1e7aff
MTelford <operator_141@outlook.com>
2021-09-05 14:46:35 +0100

    modified test_set_questions_file_path

--- tests/test_set_questions_file_path.py
+++ tests/test_set_questions_file_path.py
@@ -1,24 +1,25 @@
 import unittest
 import pathlib
+from diary_tool_main.diary_tool import set_questions_file_path
 
-
-
-from diary_tool import set_questions_file_path
 
 class TestSetQuestionsFilePath(unittest.TestCase):
-    def test_get_questions_from_user(self):
+    def test_set_questions_file_path(self):
         """
         Tests that function sets file path for questions
         correctly
         """
+        # actual path to function
+        main_path = set_questions_file_path()
 
-        result = set_questions_file_path()
-        current_test_directory = str(pathlib.Path(__file__).parent.resolve())
-        actual_function_directory = current_test_directory.replace('tests', 'questions')
-        
-        self.assertEqual(result, actual_function_directory)
+        current_directory = str(pathlib.Path(__file__).parent.resolve())
 
-        
-    
+        # our fabricated path to test against actual path in main
+        func_path = current_directory.replace(
+            'tests', 'diary_tool_main/questions')
+
+        self.assertEqual(main_path, func_path)
+
+
 if __name__ == '__main__':
     unittest.main()
