62d0aeaa38717eb102b53805bced15dbf3ccb2c7
MTelford <operator_141@outlook.com>
2021-09-05 01:46:44 +0100

    added test for function --> format_stored_questions_for_user

---
+++ tests/test_format_stored_questions_for_user.py
@@ -0,0 +1,17 @@
+import unittest
+
+from diary_tool import format_stored_questions_for_user
+
+class TestAskAndAwaitAnswer(unittest.TestCase):
+    def test_ask_and_await_answer(self):
+        """
+        Tests function that formats text
+        for user from questions file
+        """
+
+        
+
+
+
+if __name__ == '__main__':
+    unittest.main()
